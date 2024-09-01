281876d174c40a0fac8aab64e58171209c37c015
JJ Hoffmann <jonathan.hoffmann@gmail.com>
2024-09-01 14:57:15 -0700

    big-Oh

--- big-O.h
+++ big-O.h
@@ -1,8 +1,8 @@
 /************************************************************************
 CS311 Kazumi Slott
 
-Your name:
-Your program number:
+Your name: JJ Hoffmann
+Your program number: 16
 Hours spent:
 Describe any difficulties here: 
 
@@ -36,23 +36,89 @@ Please explain.
 ********************************************************************/
 
 
-//What is the runtime complexity of this algorithm? O(????)
+//What is the runtime complexity of this algorithm? O(n^3)
 //Even if you cannot get this function to work, you may guess the complexity and provide your answer above.
 int algoA(const int a[], int sz)//Don't change the function headers
-{
+{   
+    // Initialize max_sum to 0
+    int max_sum = 0;
 
+    // Check to make sure the array has elements
+    if (sz == 0) return 0;
+
+    // i == length of subarray
+    for (int i = 1; i <= sz; i++) {
+
+        // j == starting index of subarray
+        for (int j = 0; j <= sz - i; j++) {
+            int sum = 0;
+            
+            // k == index of subarray within array a[]
+            for (int k = j; k < j + i; k++) {
+                sum += a[k]; // sum of subarray
+            }
+
+            // Set the max_sum to the greater of the current sum or the max_sum 
+            max_sum = (sum > max_sum) ? sum : max_sum;
+        }
+    }
 }
 
 //What is the runtime complexity of this algorithm? O(????)
 //Even if you cannot get this function to work, you may guess the complexity and provide your answer above..
 int algoB(const int a[], int sz)
 {
+    // Initialize max_sum to 0
+    int max_sum = 0;
 
+    // Check to make sure the array has elements
+    if (sz == 0) return 0;
+
+    // i == starting position
+    for (int i = 0; i < sz; i++) {
+
+        // Initialize sum to 0 on each iteration of i
+        int sum = 0;
+
+        // j == len of subarray
+        for (int j = i; j < sz; j++) {
+
+            // Add every j element to the array
+            sum += a[j];
+
+             // Set the max_sum to the greater of the current sum or the max_sum 
+            max_sum = (sum > max_sum) ? sum : max_sum;
+        }
+    }
 }
 
-//What is the runtime complexity of this algorithm? O(????)
+//What is the runtime complexity of this algorithm? O(n)
 //Even if you cannot get this function to work, you may guess the complexity and provide your answer above..
 int algoC(const int a[], int sz)
 {
+    // Initialize max_sum to 0
+    int max_sum = 0;
+
+    // Check to make sure the array has elements
+    if (sz == 0) return 0;
+
+    // Initialize the sum to the value of the first array element 
+    int sum = a[0];
+
+    // Loop through the array
+    for (int i = 0; i < sz; i++) {
+
+        // If the current element is greater than the sum plus the current element, set the sum to the current element
+        if (a[i] > sum + a[i]) 
+            sum = a[i];
+        else 
+        // Otherwise, add the current element to the sum
+            sum = sum + a[i];
+    
+        // Set the max_sum to the greater of the current sum or the max_sum 
+        max_sum = (sum > max_sum) ? sum : max_sum;
+    }
+
+    return max_sum;
 
 }
