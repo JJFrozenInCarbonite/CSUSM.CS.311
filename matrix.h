f2c24e91318049e2358ac40d4cc0ae74c527c6c0
JJ Hoffmann <133583511+JJFrozenInCarbonite@users.noreply.github.com>
2024-09-06 15:58:04 -0700

    matrix assignment

--- matrix.h
+++ matrix.h
@@ -9,9 +9,9 @@ Replace the ?????s
 For examples of matrix multiplication
 https://matrix.reshish.com/multCalculation.php 
 
-Your name:
-Your programmer number:
-Hours spent?:
+Your name: JJ Hoffmann
+Your programmer number: 16
+Hours spent?: 
 Any difficulties?:
 ***********************************/
 #include <iostream>
@@ -85,51 +85,99 @@ Matrix<T>::Matrix(int row, int col) : R(row), C(col) //setting const R and C
 template<class T>
 ifstream& operator>>(ifstream& fin, Matrix<T>& mrx)
 {
-  /*
-  for(????????????????)
-    for(????????????????)
-      ??? >> ?????[??][??];  
+  // Read the elements of the matrix
+  for(int i = 0; i < mrx.R; i++) {
+    
+    // Read the elements of the matrix
+    for(int j = 0; j < mrx.C; j++) {
+
+      // Read the element and store it in the matrix
+      fin >> mrx.m[i][j];
+    }
+  }
+
+  // Return the stream
   return fin;
-  */
 }
 
-/*output the matrix to screen in the following format. Allocate 10 spaces for each value.
+  /*output the matrix to screen in the following format. Allocate 10 spaces for each value.
          1          1          1
          2          2          2
          3          3          3
          4          4          4 
-*/
+  */
 template<class T>
 ostream& operator<<(ostream& o, const Matrix<T>& mrx)
 {
-  /*
-   ????? //multiple lines of code here
-   //use setw(10) to allocate 10 spaces to show each value
-   //If you don't know how setw() works, you could read page 3 of my CS111 lec notes "Lecture Notes 2-3: cout, formatted outputs, fin, getline, get, ignore"
-  */
-   return o;
-  
+  // Output the matrix to the screen in the following format. Allocate 10 spaces for each value.
+  for(int i = 0; i < mrx.R; i++) {
+
+    // Output the elements of the matrix
+    for(int j = 0; j < mrx.C; j++) {
+
+      // Output the element and store it in the matrix with a width of 10
+      o << std::setw(10) << mrx.m[i][j];
+    }
+    // Add a new line
+    o << std::endl;
+  }
+  // Return the stream
+  return o;
 }
 
 //matrix multiplication
 template<class T>
 Matrix<T> Matrix<T>::operator*(const Matrix<T>& other)
 {
-  /*
-  //check if the 2 matrices are comparable. If m1 is mxn, m2 has to be nxk. 
-  if(??????????) //cannot compute because of incomparable sizes
-    ?????????; //throw an exception 
-  
-  ???? //multiple lines of code
-  */
+  // Check if the matrices are compatible for multiplication
+  if (C != other.R) {
+
+    // Throw an exception if the matrices are incompatible
+    throw std::invalid_argument("Matrices have incompatible dimensions for multiplication");
+  }
+
+  // Create a result matrix with dimensions R x other.C
+  Matrix<T> result(R, other.C);
+
+  // Perform matrix multiplication
+  for (int i = 0; i < R; ++i) {
+
+    // Perform matrix multiplication
+    for (int j = 0; j < other.C; ++j) {
+
+      // Initialize the element to 0
+      result.m[i][j] = 0; 
+
+      // Perform matrix multiplication
+      for (int k = 0; k < C; ++k) {
+
+        // Perform matrix multiplication
+        result.m[i][j] += m[i][k] * other.m[k][j];
+      }
+    }
+  }
+
+  // Return the result matrix
+  return result;
 }
 
 //destructor                                                                                                                                                                                        
 template<class T>
 Matrix<T>::~Matrix()
 {
-  //destroy what is created in heap memory                                                                                                                          
-  //if m is not NULL do the following                                   
-     //HINT: delete each dynamic array pointed to by each slot of m                                                                                                                                   
-     //delete m (m contains all rows)                                                                                                                                                                   
+  // Delete the matrix
+  if (m != nullptr) {
+
+    // Delete each row
+    for (int i = 0; i < R; ++i) {
+
+      // Delete each row
+      delete[] m[i]; 
+    }
+    // Delete the array of row pointers
+    delete[] m; 
+
+    // Set the matrix pointer to nullptr
+    m = nullptr; 
+  }
 }
