1b228859af8521be51aafd6277bf0eb26f820b5b
JJ Hoffmann <133583511+JJFrozenInCarbonite@users.noreply.github.com>
2024-09-20 15:41:54 -0700

    qsort

--- quickSort.cpp
+++ quickSort.cpp
@@ -2,15 +2,21 @@
 Templated created by Kazumi Slott
 CS311
 
-Your name:?????????
-Your programmer number: ??????
-Hours spent: ?????
-Any difficulties?:??????????
+Your name: JJ Hoffmann
+Your programmer number: 16
+Hours spent: 1.5
+Any difficulties: Using ar instead of arr only drove me slightly insane.
 ***********************************/
 #include <iostream>
+#include "swap.h"
 using namespace std;
-//?????????? include your swap.h for mySwap().  If you didn't make it for our first homework or it didn't work, you could implement it down below within this program.
 
+/**
+ * Prints the elements of an array.
+ * 
+ * @param ar[] The array to be printed.
+ * @param s The size of the array.
+ */
 void printAr(const int ar[], int s)
 {
  for(int i = 0; i < s; i++)
@@ -18,42 +24,66 @@ void printAr(const int ar[], int s)
   cout << endl;
 }
 
-/*
-//Implement mySwap() here if you didn't make mySwap() in swap.h for our first homework.
-//It is good to have the swap function so partition() won't get crowded. 
-void mySwap(?????????????)
+// Template function from swap.h
+/**
+ * Prints the elements of an array.
+ * 
+ * @param ar[] The array to be printed.
+ * @param s The size of the array.
+ *
+template <class T>
+void mySwap(T& a, T& b)
 {
+    T temp = a;
+    a = b;
+    b = temp;
 }
 */
 
-int partition(/*????????*/)
-{
-  //I have 3 variables: pivot, i and swap_index. You may have any local variables.
+/**
+ * Partitions the array around a pivot.
+ * 
+ * @param ar[] The array to be partitioned.
+ * @param start The starting index of the partition to be sorted.
+ * @param end The ending index of the partition to be sorted.
+ * 
+ * @return The index where the pivot is placed after partitioning.
+ */
+int partition(int ar[], int start, int end) {
+    int mid = (start + end) / 2;
+    mySwap(ar[mid], ar[end]);  // Swap middle element with the last
 
-  //swap the middle element with the last element first (recall this would be a good idea in case the list is sorted already).
+    int pivot = ar[end];       // Pivot is now the last element
+    int red = start;           // Red starts at the beginning
 
-  //For implementation of this function, refer to my lecture notes "quick sort". 
-  //If you used a different way, you would receive zero points.
-
-  //This function returns the index where the pivot value went in. 
-}
-
-void quickSort(int ar[], int start, int end)
-{
-  /*
-  if(????????) //If the partition has only one element or none, return. 
-                   //In other words, if the partition has at least 2 elements, go inside the if.
-                   //use start and end to figure out how many elements you have in the partition.
-    {
-      //call partition.
-      //quickSort on the left partition
-      //quickSort on the right partition
+    for (int green = start; green < end; green++) {
+        if (ar[green] < pivot) {
+            mySwap(ar[red], ar[green]);
+            red++;             // Move red pointer to the right
+        }
     }
-  */
+    mySwap(ar[red], ar[end]);   // Swap the pivot with the red pointer
+    return red;                // Return the index of the pivot
 }
 
-int main()
-{
+/**
+ * Sorts an array using the quicksort algorithm.
+ *  
+ * @param ar[] The array to be sorted.
+ * @param start The starting index of the portion of the array to be sorted.
+ * @param end The ending index of the portion of the array to be sorted.
+ */
+void quickSort(int ar[], int start, int end) {
+    if (start >= end) 
+        return;                // If the partition has 0 or 1 element, return
+    else {
+        int pivot = partition(ar, start, end);  // Partition and get pivot index
+        quickSort(ar, start, pivot - 1);        // Sort left partition
+        quickSort(ar, pivot + 1, end);          // Sort right partition
+    }
+}
+
+int main() {
   int ar[] = {15, 10, 5, 7, 1, 4, 12, 8, 6};
   quickSort(ar, 0, 8);
   printAr(ar, 9); //1 4 5 6 7 8 10 12 15
--- swap.h
+++ swap.h
@@ -1,12 +1,21 @@
 /********************************
 The following mySwap works like the swap function in the library.
 
-Your name: Jj Hoffmann
+Your name: JJ Hoffmann
 Your programmer number: 16
 Hours spent: .25
 Any difficulties?: No
 ********************************/
 
+/**
+ * Prints the elements of an array.
+ * 
+ * This function outputs all elements of an integer array, separated by spaces, followed by a newline.
+ * 
+ * @param ar[] The array to be printed.
+ * @param s The size of the array.
+ *
+ */
 template <class T>
 void mySwap(T& a, T& b)
 {
