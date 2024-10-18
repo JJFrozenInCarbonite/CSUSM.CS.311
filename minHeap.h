14ea5ba7f4949cc583df24890917d82599624670
JJ Hoffmann <133583511+JJFrozenInCarbonite@users.noreply.github.com>
2024-10-18 12:56:08 -0700

    completed priorityQ.cpp

--- minHeap.h
+++ minHeap.h
@@ -3,10 +3,10 @@ Template prepared by Kazumi Slott
 CS311
 min heap class
 
-Your name: ????
-Your programmer number: ????
+Your name: JJ Hoffmann
+Your programmer number: 16
 Hours spent making and testing your min heap class: ????
-An y difficulties?: ????
+Any difficulties?: ????
 *******************************************************/
 #ifndef MINHEAP_H
 #define MINHEAP_H
@@ -47,39 +47,193 @@ public:
 };
 
 
-//You need to implement all the memeber functions above.  Don't forget operator<<().
+/**
+ * @brief Constructs a minHeap with a specified capacity.
+ *
+ * This constructor initializes a minHeap object with a given capacity.
+ * It sets the initial number of elements in the heap to zero and allocates
+ * memory for the heap array based on the specified capacity.
+ *
+ * @tparam T The type of elements stored in the heap.
+ * @param c The maximum capacity of the heap.
+ */
+template <class T>
+minHeap<T>::minHeap(int c)
+{
+  capacity = c;
+  num = 0;
+  ar = new T[capacity];
+}
 
-//insert()
-//{
-//  if(/* array is full */)
-//    throw Overflow(); //"The array is full"; 
-//}
+/**
+ * @brief Maintains the min-heap property for the heap.
+ *
+ * This function ensures that the subtree rooted at index `i` satisfies the min-heap property.
+ * It compares the element at index `i` with its left and right children and swaps it with the
+ * smallest of the three if necessary. The function then recursively calls itself on the affected
+ * subtree to ensure the entire subtree satisfies the min-heap property.
+ *
+ * @tparam T The type of elements stored in the heap.
+ * @param i The index of the element to heapify.
+ */
+template <class T>
+void minHeap<T>::min_heapify(int i)
+{
+  int left = 2*i + 1;
+  int right = 2*i + 2;
+  int smallest = i;
 
+  if(left < num && ar[left] < ar[i])
+    smallest = left;
+  if(right < num && ar[right] < ar[smallest])
+    smallest = right;
+
+  if(smallest != i)
+  {
+    mySwap(ar[i], ar[smallest]);
+    min_heapify(smallest);
+  }
+}
+
+/**
+ * @brief Ensures the min-heap property by moving an element up the heap.
+ *
+ * This function maintains the min-heap property by comparing the element at index `i`
+ * with its parent. If the element is smaller than its parent, it swaps them and recursively
+ * calls itself on the parent index. This process continues until the element is in the correct
+ * position or it becomes the root of the heap.
+ *
+ * @tparam T The type of elements stored in the heap.
+ * @param i The index of the element to bubble up.
+ */
+template <class T>
+void minHeap<T>::bubbleUp(int i)
+{
+  int parent = (i-1)/2;
+
+  if(i > 0 && ar[i] < ar[parent])
+  {
+    mySwap(ar[i], ar[parent]);
+    bubbleUp(parent);
+  }
+}
+
+/**
+ * @brief Inserts an element into the min-heap.
+ *
+ * This function inserts a new element into the min-heap. If the heap is at full capacity,
+ * it throws an `Overflow` exception. The new element is added to the end of the heap array,
+ * and the `bubbleUp` function is called to maintain the min-heap property. The number of
+ * elements in the heap is then incremented.
+ *
+ * @tparam T The type of elements stored in the heap.
+ * @param el The element to be inserted into the heap.
+ * @throws Overflow if the heap is at full capacity.
+ */
+template <class T>
+void minHeap<T>::insert(const T& el)
+{
+  if(num == capacity)
+    throw Overflow();
+
+  ar[num] = el;
+  bubbleUp(num);
+  num++;
+}
+
+/**
+ * @brief Finds the index of a given element in the min-heap.
+ *
+ * This function searches for the specified element in the heap array. It iterates through
+ * the elements in the heap and returns the index of the first occurrence of the element
+ * that matches the given key. If the element is not found, it throws a `NotFound` exception.
+ *
+ * @tparam T The type of elements stored in the heap.
+ * @param key The element to be found in the heap.
+ * @return int The index of the element if found.
+ * @throws NotFound if the element is not found in the heap.
+ */
 template <class T>
 int minHeap<T>::find(const T& key) const
 {
-  for(int i = 0; i < num; i++) 
+  for(int i = 0; i < num; i++)
     if(ar[i] == key)
       return i;
 
-  //The element doesn't exist 
-  throw NotFound();// "The element doesn't exist";                                                                                             
+  throw NotFound();
 }
 
-//remove(int i)
-//{
-//  if(i is invalid)
-//    throw BadIndex(); //"The element doesn't exist";                                                                       
-//}
+/**
+ * @brief Removes an element from the min-heap.
+ *
+ * This function removes the element at the specified index from the heap. If the heap is empty,
+ * it throws an `Underflow` exception. If the index is out of bounds, it throws a `BadIndex` exception.
+ * The element at the specified index is replaced with the last element in the heap, and the `min_heapify`
+ * function is called to maintain the min-heap property. The number of elements in the heap is then decremented.
+ *
+ * @tparam T The type of elements stored in the heap.
+ * @param i The index of the element to be removed.
+ * @throws Underflow if the heap is empty.
+ * @throws BadIndex if the index is out of bounds.
+ */
+template <class T>
+void minHeap<T>::remove(int i) {
+  if (num <= 0) {
+    throw Underflow();
+  }
+  if (i < 0 || i >= num) {
+    throw BadIndex();
+  }
+  ar[i] = ar[num - 1];
+  num--;
+  min_heapify(i);
+}
 
-/*
-getMin()
+/**
+ * @brief Retrieves the minimum element from the min-heap.
+ * 
+ * This function returns the minimum element from the min-heap. If the heap is empty, it throws an `Underflow` exception.
+ * The minimum element is stored at the root of the heap, so it is removed from the heap using the `remove` function.
+ * The minimum element is then returned to the caller.
+ * 
+ * @tparam T The type of elements stored in the heap.
+ * @return T The minimum element in the heap.
+ * @throws Underflow if the heap is empty.
+ */
+template <class T>
+T minHeap<T>::getMin()
 {
-  if(the heap is empty)
-      throw Underflow();
-  //This function removes the top element and returns it.
-  //You should be calling remove()
+  if(num == 0)
+    throw Underflow();
+
+  T min = ar[0];
+  remove(0);
+  return min;
 }
-*/
+
+/**
+ * @brief Overloads the output stream operator to print the min-heap.
+ * 
+ * This function overloads the output stream operator to print the elements of the min-heap.
+ * It iterates through the elements in the heap and prints them to the output stream separated by spaces.
+ * 
+ * @tparam T The type of elements stored in the heap.
+ * @param o The output stream to write the heap elements to.
+ * @param h The min-heap object to be printed.
+ * @return ostream& The output stream containing the printed heap elements.
+ * 
+ */
+template <class T>
+ostream& operator<<(ostream& o, const minHeap<T>& h)
+{
+  if (h.num == 0) {
+    o << "none";
+  } 
+  else {
+    for(int i = 0; i < h.num; i++)
+      o << h.ar[i] << " ";
+  }
+  return o;
+}
 
 #endif
--- priorityQ.cpp
+++ priorityQ.cpp
@@ -7,70 +7,160 @@ If you decide to use my getNum(), you need to compile your program as follows.
 g++ -std=c++11 priorityQ.cpp //stoi requires to be compiled with c++11
 
 Program description: ?????
-Your name: ?????
-Your programmer number: ??????
+Your name: JJ Hoffmann
+Your programmer number: 16
 Hours spent making this application: ?????
 Are you rejecting invalid inputs?: ????
 Any difficulties: ???????
 ********************************************************/
 #include <iostream>
-#include <stdexcept>//for exception, invalid_argument
-#include <string>//for stoi()
+#include <string> //for stoi()
 using namespace std;
 #include "minHeap.h"
-#include "/cs/slott/cs211/checkInput.h" //You might want to check this header to see if you could use getData(min, max, errorMessage). You don't have to use it.
+//#include "/cs/slott/cs211/checkInput.h" //You might want to check this header to see if you could use getData(min, max, errorMessage). You don't have to use it.
 
-//I made a total of 5 functions in addition to main(). You can make as many functions as you need.
+
 void showMenu();
-//int getNum();
+void addPatient(int& patientCount, minHeap<int>& patientQueue);
+void nextPatient(minHeap<int>& patientQueue);
+int getNum();
+void printQueue(const minHeap<int>& patientQueue);
 
 
 const int START = 1000; //assuming this number will not go over 9999
 
 int main()
 {
-  //call showMenu() to show the menu
+  int choice; // user choice
+  int patientCount = START; // A monotonically increasing integer to be used as part of the patient ID
+  minHeap<int> patientQueue(START); // array to store patient numbers
 
-  //You need to have the following prompt somewhere. You can move it to a different function.
-  //cout << "Enter a severity, 1 most severe, .. 5 least severe: ";
+  showMenu();
+  while (choice != 3)
+    {
+      choice = getNum();
+      switch (choice) {
+        case 1:
+          addPatient(patientCount, patientQueue);
+          showMenu();
+          break;
+        case 2:
+          nextPatient(patientQueue);
+          showMenu();
+          break;
+        case 3:
+          printQueue(patientQueue);
+          break;
+        default:
+          cout << "Invalid choice. The choice should be 1, 2, or 3. Enter a number: ";
+      }
+    }
 
   return 0;
 }
 
-//Show the menu
+/**
+ * @brief Displays the main menu for the application.
+ *
+ * This function prints the main menu options to the console. The menu includes:
+ * 1. Patient Check in
+ * 2. Doctor takes a next patient
+ * 3. End
+ *
+ * The menu is displayed with a separator line at the top for better readability.
+ */
 void showMenu()
 {
   cout << "---------------------------------------------------" << endl;
   cout << "1: Patient Check in" << endl;
   cout << "2: Doctor takes a next patient" << endl;
   cout << "3: End" << endl;
+  cout << "Enter a number: ";
 }
 
-//You don't need to use this function. If you decide to use it, compile your program using -std=c++11 (check my homework instructions)
-//This function rejects non-integers. It returns an integer only.
-int getNum()
+/**
+ * @brief Prompts the user to enter a number and validates the input.
+ *
+ * This function continuously prompts the user to enter a number until a valid integer is provided.
+ * If the user enters an invalid input (non-integer), the function clears the error state and ignores
+ * the remaining characters in the input buffer before prompting the user again.
+ *
+ * @return int The valid integer entered by the user.
+ */
+int getNum() 
 {
-   string choice; //For example, when asking for 10, the user enters ab 
-   bool bad;
-   int num;
+  int num;  // user's choice
+  while (true) {
+      cin >> num;
 
-   do
-     {
-       bad = false;
-       cout << "Enter a number: ";
-       cin >> choice; 
-
-       try
-	 {
-	   num = stoi(choice); //convert a C++ string to an integer. stoi() is in string.h of c++ version 11
-	 }
-       catch(invalid_argument) //stoi() throws an exception if it cannot convert the string to an integer. E.g. "ab" cannot be an integer.
-	 {
-	   cout << "Your choice has to be a number. ";
-	   bad = true;
-	 }
-     }while(bad == true);
-
-   return num;
+      // Check if the input is a number
+      if (cin.fail()) {
+        
+        // Clear the error state
+        cout << "Your choice has to be a number. Enter a number: ";
+        cin.clear();
+        cin.ignore(1000, '\n');
+      }
+      else {
+        // Return the validated input
+        return num;
+      }
+  }
 }
 
+void addPatient(int& patientCount, minHeap<int>& patientQueue) {
+  
+  // patient severity
+  int severity;
+
+  while(true) {
+    cout << "Enter a severity, 1 most severe, .. 5 least severe: ";
+    cin >> severity;
+
+    if (cin.fail()) {
+      cout << "Your choice has to be a number. ";
+      cin.clear();
+      cin.ignore(1000, '\n');
+    }
+    else if (severity < 1 || severity > 5) {
+      cout << "Invalid choice. The choice should be 1, 2, 3, 4, or 5.";
+      cin.clear();
+      cin.ignore(1000, '\n');
+      break;
+    }
+    else {
+      break;
+    }
+  }
+
+  // validate severity entered by user
+  while (severity < 1 || severity > 5) {
+    
+    // get severity from user
+    severity = getNum();
+    if (severity < 1 || severity > 5) {
+      cout << "Invalid choice. The choice should be 1, 2, 3, 4, or 5.";
+    }
+  }
+
+  // create patient ID by concatenating severity and patient count and convert to int
+  int patientId = stoi(to_string(severity) + to_string(patientCount++));
+  patientQueue.insert(patientId);
+
+  printQueue(patientQueue);
+  
+}
+
+void nextPatient(minHeap<int>& patientQueue) {
+  try {
+    int patientId = patientQueue.getMin();
+    cout << patientId << " is going to be treated now" << endl;
+  }
+  catch (const minHeap<int>::Underflow&) {
+    cout << "The queue has none" << endl;
+  }
+}
+
+void printQueue(const minHeap<int>& patientQueue) {
+  cout << "The queue has " << patientQueue << endl;
+}
