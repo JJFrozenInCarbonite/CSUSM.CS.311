9bf4c4147954502e16bd69a58ebad6f45d88c9d7
JJ Hoffmann <133583511+JJFrozenInCarbonite@users.noreply.github.com>
2024-10-24 17:24:08 -0700

    remove alignmentCheck.cpp

--- alignmentCheck.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Alignment of int: " << __alignof__(int) << std::endl;
-    std::cout << "Alignment of double: " << __alignof__(double) << std::endl;
-    std::cout << "Alignment of pointer: " << __alignof__(void*) << std::endl;
-    return 0;
-}
