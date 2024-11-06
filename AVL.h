040477ff8c2492cf16bd15743d650f1f1b67311c
JJ Hoffmann <133583511+JJFrozenInCarbonite@users.noreply.github.com>
2024-11-05 19:08:17 -0800

    AVL

--- AVL.h
+++ AVL.h
@@ -11,8 +11,8 @@ underlying data structures. This has the benefit of simplifying the code and mak
 break the code by accident... or allow a bad actor to break the code on purpose.
 *****************/
 
-#ifndef BST_H
-#define BST_H
+#ifndef AVL_H
+#define AVL_H
 
 #include <iostream>
 #include <queue>
@@ -20,28 +20,31 @@ break the code by accident... or allow a bad actor to break the code on purpose.
 
 using namespace std;
 
-//forward declaration of BST class. We need this because BST class is used as a friend in Node. 
+//forward declaration of AVL class. We need this because AVL class is used as a friend in Node. 
 //Check your stack_L_T.h if you don't know what to put here.
 template <class T>
-class BST;
+class AVL;
 
-// Node class for the binary search tree (BST)
+// Node class for the binary search tree (AVL)
 template <class T>
 class Node {
-    friend class BST<T>;  // Grant BST access to private members of Node
+    friend class AVL<T>;  // Grant AVL access to private members of Node
 private:
     T el;               // Element stored in the node
     Node* right;        // Pointer to the right child
     Node* left;         // Pointer to the left child
+    Node* up;           // Pointer to the parent node
+    int bf;             // Balance factor
+  
 public:
-    Node() : right(nullptr), left(nullptr) {} // Default constructor
-    Node(const T& e) : el(e), right(nullptr), left(nullptr) {} // Constructor with element
+    Node() : right(nullptr), left(nullptr), up(nullptr), bf(0) {} // Default constructor
+    Node(const T& e) : el(e), right(nullptr), left(nullptr), up(nullptr), bf(0) {} // Constructor with element
     T getEl() const { return el; } // Returns the element
 };
 
-// Binary Search Tree (BST) class
+// Binary Search Tree (AVL) class
 template <class T>
-class BST 
+class AVL 
 {
 private:
   Node<T>* root; // Root of the tree
@@ -57,10 +60,14 @@ private:
   int getEvenNodes(Node<T>* p);
   Node<T>* searchR(Node<T>* p, const T& e);
   void remove(Node<T>*& p);
+  Node<T>* rightRotation(Node<T>* p);
+  Node<T>* leftRotation(Node<T>* p);
+  void balanceTree(Node<T>* p);
+  Node<T>* updateBF(Node<T>* q);
 
  public:
-  BST() { root = nullptr; }  //implement constructor here
-  ~BST(); 
+  AVL() { root = nullptr; }  //implement constructor here
+  ~AVL(); //need to have a private recursive function. Name it destroy.
   void insertNodeR(const T& e);
   void insertNodeI(const T& e);
   void inOrderPrint();
@@ -77,13 +84,13 @@ private:
   void DFTprint();
 };
 
-// Destructor for the BST class
+// Destructor for the AVL class
 /**
  * Cleans up the binary search tree by deallocating all nodes.
  * Calls the private destroy function starting from the root.
  */
 template <class T>
-BST<T>::~BST()
+AVL<T>::~AVL()
 {
   destroy(root);
 }
@@ -94,7 +101,7 @@ BST<T>::~BST()
  * @param p The current node to start the destruction process from.
  */
 template <class T>
-void BST<T>::destroy(Node<T>* p)
+void AVL<T>::destroy(Node<T>* p)
 {
   if (p == nullptr) return;   // Base case: empty node
   destroy(p->left);           // Destroy left subtree
@@ -107,7 +114,7 @@ void BST<T>::destroy(Node<T>* p)
  * @param e The element to be inserted into the tree.
  */
 template <class T>
-void BST<T>::insertNodeR(const T& e)
+void AVL<T>::insertNodeR(const T& e)
 {
   insertNodeR(root, e);
 }
@@ -118,7 +125,7 @@ void BST<T>::insertNodeR(const T& e)
  * @param e The element to be inserted into the tree.
  */
 template <class T>
-void BST<T>::insertNodeR(Node<T>*& p, const T& e) {
+void AVL<T>::insertNodeR(Node<T>*& p, const T& e) {
     if (p == nullptr) {
       p = new Node<T>(e); // Create a new node
     } else if (e < p->el) {
@@ -134,33 +141,161 @@ void BST<T>::insertNodeR(Node<T>*& p, const T& e) {
  * @param e The element to be inserted into the tree.
  */
 template <class T>
-void BST<T>::insertNodeI(const T& e) {
+void AVL<T>::insertNodeI(const T& e) {
     Node<T>* newNode = new Node<T>(e); // Create a new node to insert
+    if (root == nullptr) {
+        root = newNode; // Insert as the root if the tree is empty
+        return;
+    }
+
     Node<T>* p = root; // Current node
     Node<T>* parent = nullptr; // Parent of the current node
 
+    // Find the correct position to insert the new node
     while (p != nullptr) {
-      parent = p; // Update the parent node
-      if (e < p->el) 
-          p = p->left; // Move to the left child
-      else 
-          p = p->right; // Move to the right child
+        parent = p; // Update the parent node
+        if (e < p->el) {
+            p = p->left; // Move to the left child
+        } else {
+            p = p->right; // Move to the right child
+        }
     }
 
     // Insert the new node as a child of the parent
-    if (parent == nullptr)
-      root = newNode; // Insert as the root
-    else if (e < parent->el)
-      parent->left = newNode; // Insert as the left child
-    else
-      parent->right = newNode; // Insert as the right child
+    if (e < parent->el) {
+        parent->left = newNode; // Insert as the left child
+    } else {
+        parent->right = newNode; // Insert as the right child
+    }
+    newNode->up = parent; // Set the parent pointer of the new node
+
+    // Balance the tree starting from the parent of the new node
+    balanceTree(parent);
+}
+
+/**
+ * Updates the balance factors of the nodes starting from the given node.
+ * This function traverses up the tree from the given node, updating the
+ * balance factors and checking for imbalances.
+ *
+ * @param p The starting node from which to update the balance factors.
+ * @return The first unbalanced node encountered, or nullptr if the tree is balanced.
+ */
+templa
+template <class T>
+Node<T>* AVL<T>::updateBF(Node<T>* p) {
+    while (p != nullptr) {
+        int leftHeight = (p->left != nullptr) ? getMaxLength(p->left) : 0;
+        int rightHeight = (p->right != nullptr) ? getMaxLength(p->right) : 0;
+
+        p->bf = rightHeight - leftHeight;
+
+        if (p->bf < -1 || p->bf > 1) {
+            return p; // Return the unbalanced node
+        }
+
+        p = p->up; // Move up to the parent node
+    }
+    return nullptr; // Tree is balanced
+}
+
+/**
+ * Balances the AVL tree starting from the given node.
+ * This function updates the balance factors of the nodes and performs
+ * the necessary rotations to maintain the AVL tree properties.
+ *
+ * @param p The starting node from which to balance the tree.
+ */
+template <class T>
+void AVL<T>::balanceTree(Node<T>* p) {
+    while (p != nullptr) {
+        updateBF(p); // Update the balance factor of the current node
+
+        if (p->bf == -2) { // Left heavy
+            if (p->left->bf == 1) {
+                leftRotation(p->left); // Left-right case
+            }
+            rightRotation(p); // Left-left case
+        } else if (p->bf == 2) { // Right heavy
+            if (p->right->bf == -1) {
+                rightRotation(p->right); // Right-left case
+            }
+            leftRotation(p); // Right-right case
+        }
+
+        p = p->up; // Move up to the parent node
+    }
+}
+
+/**
+ * Performs a left rotation on the subtree rooted at the given node.
+ * This operation is used to balance the AVL tree when the right subtree
+ * is heavier than the left subtree.
+ *
+ * @param p The root of the subtree to be rotated.
+ * @return The new root of the rotated subtree.
+ */
+template <class T>
+Node<T>* AVL<T>::leftRotation(Node<T>* p) {
+    Node<T>* newRoot = p->right; // The right child will become the new root
+    p->right = newRoot->left; // The left child of the new root becomes the right child of the old root
+    if (newRoot->left != nullptr) {
+        newRoot->left->up = p; // Update the parent pointer of the new right child
+    }
+    newRoot->up = p->up; // The parent of the old root becomes the parent of the new root
+    if (p->up == nullptr) {
+        root = newRoot; // If the old root was the root of the entire tree, update the tree's root
+    } else if (p == p->up->left) {
+        p->up->left = newRoot; // If the old root was a left child, update the parent's left child
+    } else {
+        p->up->right = newRoot; // If the old root was a right child, update the parent's right child
+    }
+    newRoot->left = p; // The old root becomes the left child of the new root
+    p->up = newRoot; // Update the parent pointer of the old root
+
+    updateBF(p); // Update the balance factor of the old root
+    updateBF(newRoot); // Update the balance factor of the new root
+
+    return newRoot; // Return the new root of the rotated subtree
+}
+
+/**
+ * Performs a right rotation on the subtree rooted at the given node.
+ * This operation is used to balance the AVL tree when the left subtree
+ * is heavier than the right subtree.
+ *
+ * @param p The root of the subtree to be rotated.
+ * @return The new root of the rotated subtree.
+ */
+template <class T>
+Node<T>* AVL<T>::rightRotation(Node<T>* p) {
+    Node<T>* newRoot = p->left; // The left child will become the new root
+    p->left = newRoot->right; // The right child of the new root becomes the left child of the old root
+    if (newRoot->right != nullptr) {
+        newRoot->right->up = p; // Update the parent pointer of the new left child
+    }
+    newRoot->up = p->up; // The parent becomes the parent of the new root
+    if (p->up == nullptr) {
+        root = newRoot; // If the old root was the root of the entire tree, update the tree's root
+    } else if (p == p->up->left) {
+        p->up->left = newRoot; // If the old root was a left child, update the parent's left child
+    } else {
+        p->up->right = newRoot; // If the old root was a right child, update the parent's right child
+    }
+    newRoot->right = p; // The old root becomes the right child of the new root
+    p->up = newRoot; // Update the parent pointer
+
+    updateBF(p); // Update the balance factor of the old root
+    updateBF(newRoot); // Update the balance factor of the new root
+
+    return newRoot; // Return the new root
 }
 
 /**
  * Prints the elements of the binary search tree in in-order traversal.
  */
 template <class T>
-void BST<T>::inOrderPrint()
+void AVL<T>::inOrderPrint()
 {
   inOrderPrint(root);
 }
@@ -170,7 +305,7 @@ void BST<T>::inOrderPrint()
  * @param p The current node to start the in-order traversal from.
  */
 template <class T>
-void BST<T>::inOrderPrint(Node<T>* p) {
+void AVL<T>::inOrderPrint(Node<T>* p) {
   if (p == nullptr) return; // Base case: empty tree
   inOrderPrint(p->left);    // Traverse left subtree
   cout << p->el << "-->";   // Print the element
@@ -181,7 +316,7 @@ void BST<T>::inOrderPrint(Node<T>* p) {
  * Prints the elements of the binary search tree in pre-order traversal.
  */
 template <class T>
-void BST<T>::preOrderPrint() {
+void AVL<T>::preOrderPrint() {
   preOrderPrint(root);
 }
 
@@ -190,7 +325,7 @@ void BST<T>::preOrderPrint() {
  * @param p The current node to start the pre-order traversal from.
  */
 template <class T>
-void BST<T>::preOrderPrint(Node<T>* p) {
+void AVL<T>::preOrderPrint(Node<T>* p) {
   if (p == nullptr) return; // Base case: empty tree
   cout << p->el << "-->";   // Print the element
   preOrderPrint(p->left);   // Traverse left subtree
@@ -201,7 +336,7 @@ void BST<T>::preOrderPrint(Node<T>* p) {
  * Prints the elements of the binary search tree in post-order traversal.
  */
 template <class T>
-void BST<T>::postOrderPrint() {
+void AVL<T>::postOrderPrint() {
   postOrderPrint(root);
 }
 
@@ -210,7 +345,7 @@ void BST<T>::postOrderPrint() {
  * @param p The current node to start the post-order traversal from.
  */
 template <class T>
-void BST<T>::postOrderPrint(Node<T>* p) {
+void AVL<T>::postOrderPrint(Node<T>* p) {
   if (p == nullptr) return; // Base case: empty tree
   postOrderPrint(p->left);  // Traverse left subtree
   postOrderPrint(p->right); // Traverse right subtree
@@ -222,7 +357,7 @@ void BST<T>::postOrderPrint(Node<T>* p) {
  * @return The number of nodes in the binary search tree.
  */
 template <class T>
-int BST<T>::getNumNodes() {
+int AVL<T>::getNumNodes() {
   return getNumNodes(root);
 }
 
@@ -232,7 +367,7 @@ int BST<T>::getNumNodes() {
  * @return The number of nodes in the binary search tree.
  */
 template <class T>
-int BST<T>::getNumNodes(Node<T>* p) {
+int AVL<T>::getNumNodes(Node<T>* p) {
   if (p == nullptr) return 0; // Base case: empty tree
   return 1 + getNumNodes(p->left) + getNumNodes(p->right); // Count nodes
 }
@@ -243,7 +378,7 @@ int BST<T>::getNumNodes(Node<T>* p) {
  * @return The maximum length from the root of the binary search tree.
  */
 template <class T>
-int BST<T>::getMaxLength() {
+int AVL<T>::getMaxLength() {
   return getMaxLength(root);
 }
 
@@ -253,7 +388,7 @@ int BST<T>::getMaxLength() {
  * @return The maximum length from the given node.
  */
 template <class T>
-int BST<T>::getMaxLength(Node<T>* p) {
+int AVL<T>::getMaxLength(Node<T>* p) {
   if (p == nullptr) return 0; // Base case: empty tree
   int leftLength = getMaxLength(p->left); // Length of left subtree
   int rightLength = getMaxLength(p->right); // Length of right subtree
@@ -265,7 +400,7 @@ int BST<T>::getMaxLength(Node<T>* p) {
  * @return The minimum length from the root of the binary search tree to the nearest leaf node.
  */
 template <class T>
-int BST<T>::getMinLength() {
+int AVL<T>::getMinLength() {
   return getMinLength(root);
 }
 
@@ -275,7 +410,7 @@ int BST<T>::getMinLength() {
  * @return The minimum length from the given node to the nearest leaf node.
  */
 template <class T>
-int BST<T>::getMinLength(Node<T>* p) {
+int AVL<T>::getMinLength(Node<T>* p) {
   if (p == nullptr) return 0; // Base case: empty tree
   if (p->left == nullptr) return 1 + getMinLength(p->right);
   if (p->right == nullptr) return 1 + getMinLength(p->left);
@@ -289,7 +424,7 @@ int BST<T>::getMinLength(Node<T>* p) {
  * @return The number of even elements in the binary search tree.
  */
 template <class T>
-int BST<T>::getEvenNodes() {
+int AVL<T>::getEvenNodes() {
     return getEvenNodes(root);
 }
 
@@ -299,7 +434,7 @@ int BST<T>::getEvenNodes() {
  * @return The number of even elements in the binary search tree.
  */
 template <class T>
-int BST<T>::getEvenNodes(Node<T>* p) {
+int AVL<T>::getEvenNodes(Node<T>* p) {
     if (p == nullptr) return 0; // Base case: empty tree
     int count = (p->el % 2 == 0) ? 1 : 0; // Increment count if even
     return count + getEvenNodes(p->left) + getEvenNodes(p->right); // Count evens
@@ -311,7 +446,7 @@ int BST<T>::getEvenNodes(Node<T>* p) {
  * @return A pointer to the node containing the element if found, nullptr otherwise.
  */
 template <class T>
-Node<T>* BST<T>::searchI(const T& e) {
+Node<T>* AVL<T>::searchI(const T& e) {
     Node<T>* p = root; // Start at the root
     while (p != nullptr) {
         if (e == p->el) return p; // Element found
@@ -327,7 +462,7 @@ Node<T>* BST<T>::searchI(const T& e) {
  * @return A pointer to the node containing the element if found, nullptr otherwise.
  */
 template <class T>
-Node<T>* BST<T>::searchR(const T& e) {
+Node<T>* AVL<T>::searchR(const T& e) {
     return searchR(root, e);
 }
 
@@ -338,14 +473,14 @@ Node<T>* BST<T>::searchR(const T& e) {
  * @return A pointer to the node containing the element if found, nullptr otherwise.
  */
 template <class T>
-Node<T>* BST<T>::searchR(Node<T>* p, const T& e) {
+Node<T>* AVL<T>::searchR(Node<T>* p, const T& e) {
     if (p == nullptr || p->el == e) return p; // Base case: empty tree or element found
     return (e < p->el) ? searchR(p->left, e) : searchR(p->right, e); // Recursively search
 }
 
 //This function returns true if e was removed or false if not because it doesn't exist.
 template <class T>
-bool BST<T>::remove(const T& e) //public function
+bool AVL<T>::remove(const T& e) //public function
 {
     Node<T>* del = root; // del will point to the node to be deleted
     Node<T>* parent = nullptr; // parent will point to the parent of the node to be deleted
@@ -381,7 +516,7 @@ bool BST<T>::remove(const T& e) //public function
 //p comes out of the parent node and points to the node to be deleted OR p points to root if root is the one to be deleted.
 //p will point to the grandchild (child node of the node to be deleted) if the node to be deleted has only one child or will point to NULL if p is root or the node to be deleted has no children. p will change. That is why we need to use &.                  
 template <class T>
-void BST<T>::remove(Node<T>*& p) //private function
+void AVL<T>::remove(Node<T>*& p) //private function
 {
     Node<T>* temp = p; // temp points to the node to be deleted initially
     Node<T>* t_parent; // t_parent will point to the parent node of temp's node
@@ -421,7 +556,7 @@ void BST<T>::remove(Node<T>*& p) //private function
  * Prints the elements of the binary search tree in breadth-first traversal.
  */
 template <class T>
-void BST<T>::BFTprint() {
+void AVL<T>::BFTprint() {
     if (root == nullptr) return; // If the tree is empty, return immediately
     std::queue<Node<T>*> q; // Create a queue for traversal
     q.push(root); // Start with the root node
@@ -439,7 +574,7 @@ void BST<T>::BFTprint() {
  * Prints the elements of the binary search tree in depth-first traversal.
  */
 template <class T>
-void BST<T>::DFTprint() {
+void AVL<T>::DFTprint() {
     if (root == nullptr) return; // If the tree is empty, return immediately
     std::stack<Node<T>*> stack; // Create a stack for traversal
     stack.push(root); // Start with the root node
