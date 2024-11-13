c8f901d10883bad272c705a12b4cedbdf4db7874
JJ Hoffmann <133583511+JJFrozenInCarbonite@users.noreply.github.com>
2024-11-13 15:11:54 -0800

    graph part 1 completed

--- graph.h
+++ graph.h
@@ -2,9 +2,9 @@
 Templated created by Kazumi Slott
 CS311
 
-Your name: ???
-Your programmer number: ???
-Hours spent: ???
+Your name: JJ Hoffmann
+Your programmer number: 16
+Hours spent: 
 Any difficulties?:
 *****************************/
 #ifndef GRAPH_H
@@ -12,60 +12,58 @@ Any difficulties?:
 #include <iostream>
 #include <queue>
 #include <list>
-using namespace std;
 
-class edge
-{
-  //????????????;  //What do you need here if you want to access the private members of edge class from graph class
-  int neighbor; //adjacent vertex
-  int wt; //weight of the edge
+class edge {
+  friend class graph; // Allows the graph class to access private members of edge
+  int neighbor;       // Adjacent vertex
+  int wt;             // Weight of the edge
  public:
   edge() { neighbor = -999, wt = -999;};
-  edge(int u, int w) {/* set data members */};
+  edge(int u, int w) { neighbor = u; wt = w; };
 };
 
-class graph
-{
-  int num_ver; //the total number of vertices in the graph
-  list<edge*>* ver_ar; //pointer to the dynamic array that holds linked lists. The element of a node in a linked list is a pointer to an edge object 
-                       //Let's use the library's list class. It is actually a doubly linked list. 
+class graph {
+  int num_ver; // Total number of vertices in the graph
+  std::list<edge*>* ver_ar; // Pointer to the dynamic array that holds linked lists. The element of a node in a linked list is a pointer to an edge object 
+                       // Let's use the library's list class. It is actually a doubly linked list. 
   int nextUnvisitedNodes(int* num, int start, int s);
-  void DFT_helper(int v, int& i, int* num, queue<string>& q);
- public:
-  graph(int V);
-  ~graph();
-  void addEdge(int v, int u, int w=1);  //If the weight is not given, 1 is used for wt.
-  void BFT(int start);
-  void DFT(int start);
+  void DFT_helper(int v, int& i, int* num, std::queue<std::string>& q);
+  public:
+    graph(int V);
+    ~graph();
+    void addEdge(int v, int u, int w = 1);  //If the weight is not given, 1 is used for wt.
+    void BFT(int start);
+    void DFT(int start);
 };
 
-//constructor: num is the number of vertices
-graph::graph(int num)
-{
-  //make a dynamic array with num slots where each slot holds a list object. 
-  //The element of a node in the linked list is a pointer to an edge object 
+// Constructor: num is the number of vertices
+graph::graph(int num) : num_ver(num){
+
+  // Allocate a dynamic array of lists, each holding pointers to edge objects
+  ver_ar = new list<edge*>[num_ver];
 }
 
-graph::~graph()
-{
-  //destroy all the edge objects created in heap                                                                                                        
-  //For each vertex in ver_ar
-  //    go through each node in the linked list. The element field of each node points to an edge object created with "new". You need to delete these.  
+// Destructor for the graph class
+graph::~graph() {
 
-  //The library's list class has a destructor. All nodes belonging to the list will be destroyed by the destructor.
-  //https://www.cplusplus.com/reference/list/list/~list/
+  // Iterate over each vertex's adjacency list
+  for (int i = 0; i < num_ver; ++i) {
 
-  //destroy the ver_ar dynamic array
+    // For each edge pointer in the list, delete the edge object
+    for (auto edge_ptr : ver_ar[i]) {
+      delete edge_ptr;
+    }
+  }
+
+  // Delete the dynamic array of lists
+  delete[] ver_ar;
 }
 
-
-//add an edge from v to u with weight w into the graph
-void graph::addEdge(int v, int u, int w)
-{
-  //We are using the list class from the library. Which member function do you need to use to add a node at the end of the list?
-  //https://www.cplusplus.com/reference/list/list/
-  //Don't create an edge object statically, then it would get destroyed as soon as you leave this function. You need to create an edge object dymamically in heap, which will remain in heap even after you leave this function.. Remember "new" operator returns the memory address of the newly created object.
-  //I have one line of code in this function.
+// Add an edge between vertices v and u with weight w
+void graph::addEdge(int v, int u, int w) {
+  
+  // Add a new edge to the adjacency list of vertex v
+  ver_ar[v].push_back(new edge(u, w));
 }
 
 //I decided to make this function a stand alone function. It doesn't have anything to do with the graph class.                                
@@ -79,77 +77,162 @@ void displayQueue(queue<T>& q)
     }
 }
 
-//start Traversal at start
-void graph::DFT(int start)
-{
-  //The algorithm is in my lecture notes.
-  //I made dynamic array called "num"
-  //I created a queue object of string data type from the library's queue class to save all the paths.
+// Start is the index for the start vertex
+void graph::DFT(int start) {
 
-  //I used do-while
-  //I am calling nextUnvisitedNodes() to see if there are more unvisited nodes.
+  // Dynamic array to track visited vertices
+  int* num = new int[num_ver]();
 
+  // Queue to store paths for display
+  std::queue<string> paths;
 
-  //You could call displayQueue() implemented above to display all the paths. 
+  // Start the DFT traversal from the given start vertex
+  int i = 0;
+  DFT_helper(start, i, num, paths);
 
-  //Don't forget to destroy the dynamic array
-}
-
-//I made the following function to see if there are still unvisited nodes. Start looking at s, which is the one after the vertext we started last time. start point may not be 0. If start was 5, check the num array from index 6 then 7... until a zero is found (you may find a zero at index 1). Next time check from index 2.                                                                                            
-//num is the array of vertices                                                                                                          
-//start is the start vertex                                                                      
-//s is the vertext right after the vertex we started last time                                                                          
-int graph::nextUnvisitedNodes(int* num, int start, int s)
-{
-  //go through the num array from s to see if you find a vertext with num=0. If so, return the index. If all visited, return -1.
-  //don't start from index 0!
-  //Althought this function may be called multiple times because there may be multiple disconnected graphs, we are checking each vertex only once.
-}
-
-void graph::DFT_helper(int v, int& i, int* num, queue<string>& q)
-{
-  //The algorithm is in my lecture notes
-
-  //The following shows how to iterate the library's linked list. You use an iterator.
-  //https://www.cplusplus.com/reference/list/list/end/ 
-  //iterator class is nested inside list class
-  for (list<??????>::iterator u = ???????.begin(); u != ??????.end(); u++)
-    {
-      //From the example on https://www.cplusplus.com/reference/list/list/end/
-      //Notice *u gives you the element of the node pointed to by u. The element is a pointer to an edge object.
-      //Between * and -> operator, -> has higher precedence. You need to use () around *u to get the element first.  
-
-      //push each path e.g. 0->1 into q. to_string(int) converts int to string. 
-      //to_string() is in c++11, so compile with the option. g++ -std=c++11 graphClient.cpp 
-      q.push(to_string(v) + "->" + to_string((*u)->neighbor));
-      
+  // Visit any disconnected components using do-while
+  int next;
+  do {
+    next = nextUnvisitedNodes(num, start, start + 1);
+    if (next != -1) {
+      DFT_helper(next, i, num, paths);
     }
+  } while (next != -1);
 
-  /***********************************************************************************************
-   **********************************************************************************************
-    Compile with a c++11 option if you are using to_string()
-        g++ -std=c++11 graphClient.cpp 
-   **********************************************************************************************
-   ***********************************************************************************************/
+  // Display all paths found during traversal
+  displayQueue(paths);
+
+  // Clean up the dynamic array
+  delete[] num;
+}
+
+// Find the next unvisited vertex in the graph
+int graph::nextUnvisitedNodes(int* num, int start, int s) {
+
+  // Loop from s to the end of the vertices
+  for (int i = s; i < num_ver; ++i) {
+    if (num[i] == 0) { // If the vertex is unvisited
+      return i; // Return the index of the first unvisited vertex
+    }
+  }
+
+  // If no unvisited vertex is found from s to the end, loop from the beginning to start
+  for (int i = 0; i < start; ++i) {
+    if (num[i] == 0) { // If the vertex is unvisited
+      return i; // Return the index of the first unvisited vertex
+    }
+  }
+
+  // If all vertices are visited, return -1
+  return -1;
+}
+
+// Recursive helper function for DFT traversal
+void graph::DFT_helper(int v, int& i, int* num, queue<string>& q) {
+
+  // Mark the current vertex as visited
+  num[v] = ++i;
+
+  // Print the vertex as it's visited
+  cout << v << " ";
+
+  // Iterate through the adjacency list of vertex v using an iterator
+  for (list<edge*>::iterator u = ver_ar[v].begin(); u != ver_ar[v].end(); ++u) {
+    
+    // Check if the neighbor vertex has been visited
+    if (num[(*u)->neighbor] == 0) {
+
+      // Push the path "v->neighbor" into the queue
+      q.push(std::to_string(v) + "->" + std::to_string((*u)->neighbor));
+
+      // Recursively visit the neighbor
+      DFT_helper((*u)->neighbor, i, num, q);
+    }
+  }
 }
 
 //start is the index for the start vertex
-void graph::BFT(int start)
-{
-  //The algorithm is in my lecture notes
+void graph::BFT(int start) {
 
-  //Use the queue class from the library
-  queue<int> q;
-  //I used another queue of strings to save all the paths. 
+  // Dynamic array to track visited vertices
+  int* num = new int[num_ver]();
 
-  //I used do-while to visit all unconnected graphs. Call nextUnvisitedNodes() to check to see if there are more unvisited vertices.
+  // Queue for breadth-first traversal
+  std::queue<int> q;
 
-  //Check the specification to figure out what member functions of the queue class you need to use.
-  // https://www.cplusplus.com/reference/queue/queue/
+  // Queue to store paths for display
+  std::queue<std::string> paths;
 
-  //You could call show all paths by calling displayQueue()                                                                     
+  // Initialize the traversal by marking the start vertex as visited and adding it to the queue
+  int i = 1;
+  num[start] = i;
+  q.push(start);
+  
+  // Display the start vertex
+  std::cout << start << " ";
 
-  //Don't forget to destroy the dynamic array if you used one
+  // Perform the BFS
+  while (!q.empty()) {
+    int v = q.front();
+    q.pop();
+
+    // Iterate through all adjacent vertices of v
+    for (auto edge_ptr : ver_ar[v]) {
+      int neighbor = edge_ptr->neighbor;
+
+      // If the neighbor hasn't been visited, visit it
+      if (num[neighbor] == 0) {
+        num[neighbor] = ++i;
+        q.push(neighbor);
+
+        // Print the visited vertex
+            std::cout << neighbor << " ";
+
+        // Record the path "v->neighbor" for display
+        paths.push(to_string(v) + "->" + to_string(neighbor));
+      }
+    }
+  }
+
+  // Visit any remaining disconnected components using do-while loop
+  int next;
+  do {
+    next = nextUnvisitedNodes(num, start, start + 1);
+    if (next != -1) {
+      num[next] = ++i;
+      q.push(next);
+
+      // Print the next component's start vertex
+      std::cout << next << " ";
+
+      // Continue BFS for the disconnected component
+      while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+
+        for (auto edge_ptr : ver_ar[v]) {
+          int neighbor = edge_ptr->neighbor;
+
+          if (num[neighbor] == 0) {
+            num[neighbor] = ++i;
+            q.push(neighbor);
+
+            // Print the visited vertex
+            std::cout << neighbor << " ";
+
+            // Record the path "v->neighbor" for display
+            paths.push(to_string(v) + "->" + to_string(neighbor));
+          }
+        }
+      }
+    }
+  } while (next != -1);
+
+  // Display all recorded paths
+  displayQueue(paths);
+
+  // Clean up the dynamic array
+  delete[] num;
 }
 
 #endif
