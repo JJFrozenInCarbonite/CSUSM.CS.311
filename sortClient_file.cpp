3d5a8dbfe48590179b97758106b8f91d945988fb
JJ Hoffmann <jonathan.hoffmann@gmail.com>
2024-12-03 17:45:00 -0800

    run multiple tests

--- sortClient_file.cpp
+++ sortClient_file.cpp
@@ -1,161 +1,137 @@
-/********************************************************************************
-CS311
-Kazumi Slott
-
-This program tells how long it takes for each of your sorting algorithms to sort an 
-array with a different number of elements.
-*********************************************************************************/
-#define TN 400
 #include <iostream>
 #include <fstream>
-#include <ctime> // for clock()
-#include <cstdlib> //for exit()
+#include <ctime>
+#include <cstdlib>
 #include <vector>
-using namespace std;
+#include <algorithm> 
 #include "sort.h"
 #include "heapSort2.h"
 #include "/cs/slott/cs211/checkInput.h" //for getData()
 
+using namespace std;
 
+vector<double> times;
 
-void runSort(char sort, int ar[])
-{
-  clock_t start, end;
-  ifstream fin;
-
-  cout << "How many numbers do you want to put into the array?: ";
-  //the number of elements we can accept is 1 to 1 million. The input file has 1 million numbers. You might want to open it to see.
-  int s = getData(1, 1000000, "Please enter a number between 1 and 1000000: ");//getData() is in /cs/slott/cs211/checkInput.h
-
-  fin.open("/cs/slott/cs311/sort.in"); //sort.in has 1000000 numbers
-  if(!fin)
-    {
-      cout << "The input file doesn't open" << endl;
-      exit(1); //ending the program
+void measureExecutionTime(void (*sortFunction)(int[], int), int ar[], int original[], int size, int numTests) {
+    times.resize(numTests);
+    clock_t start, end;
+    for (int i = 0; i < numTests; i++) {
+        cout << "Test: " << (i + 1) << endl;
+        std::copy(original, original + size, ar);
+        start = clock();
+        sortFunction(ar, size);
+        end = clock();
+        double time = (end - start) / double(CLOCKS_PER_SEC) * 1000;
+        cout << "Elapsed time: " << time << " milliseconds" << endl;
+        times[i] = time;
     }
 
-  //fill the array
-  for(int i = 0; i < s; i++)
-    fin >> ar[i]; 
-  fin.close();
-
-  cout << "\n============ Testing ";
-  switch(sort)
-    {
-    case 'S':  
-    case 's':  cout << " selection sort ";
-              vector<double> times(TN);
-              for (int i = 0; i < times.size(); i++) {
-                cout << "Test: " << (i + 1) << endl;
-                start = clock();
-                selectionSort(ar, s);
-                end = clock();
-                double time = (end - start) / double(CLOCKS_PER_SEC) * 1000;
-                cout << "Elapsed time: " << time << " milliseconds" << endl;
-                times[i] = time;
-              }
-              break;
-    case 'B':  
-    case 'b':  cout << " bubble sort ";
-              vector<double> times(TN);
-              for (int i = 0; i < times.size(); i++) {
-              cout << "Test: " << (i + 1) << endl;
-                start = clock();
-                bubbleSort(ar, s);
-                end = clock();
-                double time = (end - start) / double(CLOCKS_PER_SEC) * 1000;
-                cout << "Elapsed time: " << time << " milliseconds" << endl;
-                times[i] = time;
-              }
-              break;
-    case 'I':  
-    case 'i':  cout << " insertion sort ";
-              vector<double> times(TN);
-              for (int i = 0; i < times.size(); i++) {
-              cout << "Test: " << (i + 1) << endl;
-                start = clock();
-                insertionSort(ar, s);
-                end = clock();
-                double time = (end - start) / double(CLOCKS_PER_SEC) * 1000;
-                cout << "Elapsed time: " << time << " milliseconds" << endl;
-                times[i] = time;
-              }
-               break;
-    case 'Q':  
-    case 'q':  cout << " quick sort ";
-              vector<double> times(TN);
-              for (int i = 0; i < times.size(); i++) {
-              cout << "Test: " << (i + 1) << endl;
-                start = clock();
-                quickSort(ar, 0, s - 1);
-                end = clock();
-                double time = (end - start) / double(CLOCKS_PER_SEC) * 1000;
-                cout << "Elapsed time: " << time << " milliseconds" << endl;
-                times[i] = time;
-              }
-               break;
-    case 'M':  
-    case 'm':  cout << " merge sort ";
-              vector<double> times(TN);
-              for (int i = 0; i < times.size(); i++) {
-              cout << "Test: " << (i + 1) << endl;
-                start = clock();
-                mergeSort(ar, 0, s - 1);
-                end = clock();
-                double time = (end - start) / double(CLOCKS_PER_SEC) * 1000;
-                cout << "Elapsed time: " << time << " milliseconds" << endl;
-                times[i] = time;
-              }
-               break;
-
-    case 'H':  cout << " heap sort ";
-              vector<double> times(TN);
-              for (int i = 0; i < times.size(); i++) {
-              cout << "Test: " << (i + 1) << endl;
-                start = clock();
-                heapSort(ar, s);
-                end = clock();
-                double time = (end - start) / double(CLOCKS_PER_SEC) * 1000;
-                cout << "Elapsed time: " << time << " milliseconds" << endl;
-                times[i] = time;
-              }
-               break;
-    }
-
-  cout << " with " << s << " numbers" << endl;
-  double average_time;
-  for(int i = 0; i < times.size(); i++)
-    average_time += times[i];
-  average_time /= (double)times.size();
-  cout << "Elapsed time: " << average_time << " milliseconds" << endl;
-}  
-
-int main()
-{
-  const int MAX = 1000000; //the input file has 1000000 numbers
-  int ar[MAX]; //Make an array here instead of runSort() so it won't get destroyed after each call to runSort()
-  char choice;//choice of sorting type
-
-  do
-    {
-      cout << "---------------------------------------------" << endl;
-      cout << "Enter S for selection sort: " << endl;
-      cout << "      B for bubble sort: " << endl;
-      cout << "      I for insertion sort: " << endl;
-      cout << "      Q for quick sort: " << endl;
-      cout << "      M for merge sort: " << endl;
-      cout << "      E for end the program: " << endl;
-      cout << "      H for heap sort: " << endl;
-      cout << "Please enter your choice: ";
-      cin >> choice; 
-
-      choice = toupper(choice); //making a lowercase letter to upper case by calling the library's toupper()
-      if(choice == 'S' || choice == 'B' || choice == 'I' || choice == 'Q' || choice == 'M' || choice == 'H')
-	runSort(choice, ar);     
-      else if(choice != 'E')
-	cout << "!!!!!!!!!!!!!!!!!!!! " << choice << " is INVALID !!!!!!!!!!!!!!!!!!" << endl;
-    }while(choice != 'E');
-     
-  return 0;
+    double average_time = 0;
+    for (double time : times)
+        average_time += time;
+    average_time /= times.size();
+    cout << "Average elapsed time: " << average_time << " milliseconds" << endl;
 }
 
+void runSelectionSort(int ar[], int original[], int size, int numTests) {
+    measureExecutionTime(selectionSort, ar, original, size, numTests);
+}
+
+void runBubbleSort(int ar[], int original[], int size, int numTests) {
+    measureExecutionTime(bubbleSort, ar, original, size, numTests);
+}
+
+void runInsertionSort(int ar[], int original[], int size, int numTests) {
+    measureExecutionTime(insertionSort, ar, original, size, numTests);
+}
+
+void runQuickSort(int ar[], int original[], int size, int numTests) {
+    measureExecutionTime([](int ar[], int size) { quickSort(ar, 0, size - 1); }, ar, original, size, numTests);
+}
+
+void runMergeSort(int ar[], int original[], int size, int numTests) {
+    measureExecutionTime([](int ar[], int size) { mergeSort(ar, 0, size - 1); }, ar, original, size, numTests);
+}
+
+void runHeapSort(int ar[], int original[], int size, int numTests) {
+    measureExecutionTime(heapSort, ar, original, size, numTests);
+}
+
+void runSort(char sort, int ar[], int original[]) {
+    ifstream fin;
+
+    cout << "How many numbers do you want to put into the array?: ";
+    int s = getData(1, 1000000, "Please enter a number between 1 and 1000000: ");
+
+    cout << "How many times do you want to run the test?: ";
+    int numTests = getData(1, 1000, "Please enter a number between 1 and 1000: ");
+
+    fin.open("/cs/slott/cs311/sort.in");
+    if (!fin) {
+        cerr << "The input file doesn't open" << endl;
+        exit(1);
+    }
+
+    for (int i = 0; i < s; i++)
+        fin >> original[i];
+    fin.close();
+
+    cout << "\n============ Testing ";
+    switch (sort) {
+    case 'S':
+        cout << "selection sort with " << s << " numbers" << endl;
+        runSelectionSort(ar, original, s, numTests);
+        break;
+    case 'B':
+        cout << "bubble sort with " << s << " numbers" << endl;
+        runBubbleSort(ar, original, s, numTests);
+        break;
+    case 'I':
+        cout << "insertion sort with " << s << " numbers" << endl;
+        runInsertionSort(ar, original, s, numTests);
+        break;
+    case 'Q':
+        cout << "quick sort with " << s << " numbers" << endl;
+        runQuickSort(ar, original, s, numTests);
+        break;
+    case 'M':
+        cout << "merge sort with " << s << " numbers" << endl;
+        runMergeSort(ar, original, s, numTests);
+        break;
+    case 'H':
+        cout << "heap sort with " << s << " numbers" << endl;
+        runHeapSort(ar, original, s, numTests);
+        break;
+    default:
+        cerr << "Invalid sorting option" << endl;
+        break;
+    }
+}
+
+int main() {
+    const int MAX = 1000000;
+    int ar[MAX];
+    int original[MAX];
+    char choice;
+
+    do {
+        cout << "---------------------------------------------" << endl;
+        cout << "Enter S for selection sort: " << endl;
+        cout << "      B for bubble sort: " << endl;
+        cout << "      I for insertion sort: " << endl;
+        cout << "      Q for quick sort: " << endl;
+        cout << "      M for merge sort: " << endl;
+        cout << "      H for heap sort: " << endl;
+        cout << "      E to end the program: " << endl;
+        cout << "Please enter your choice: ";
+        cin >> choice;
+
+        choice = toupper(choice);
+        if (choice == 'S' || choice == 'B' || choice == 'I' || choice == 'Q' || choice == 'M' || choice == 'H')
+            runSort(choice, ar, original);
+        else if (choice != 'E')
+            cout << "!!!!!!!!!!!!!!!!!!!! " << choice << " is INVALID !!!!!!!!!!!!!!!!!!" << endl;
+    } while (choice != 'E');
+
+    return 0;
+}
